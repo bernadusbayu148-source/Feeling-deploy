89d52d0b52c0f7f139b24970d4db37891992cffa
bernadusbayu148-source <bernadus.bayu148@gmail.com>
2026-04-27 10:57:44 +0700

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -5,7 +5,7 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCs6TaK4wUa-Xf0JkbL4my61JxJlX5CMUM",
   authDomain: "manupi-web.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  databaseURL: "https://manupi-web-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "manupi-web",
   storageBucket: "manupi-web.firebasestorage.app",
   messagingSenderId: "399169759168",
