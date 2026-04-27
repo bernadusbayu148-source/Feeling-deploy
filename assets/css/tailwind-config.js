1702615a4583455fc927f98afc840493f9151ae4
bernadusbayu148-source <bernadus.bayu148@gmail.com>
2026-04-27 07:40:24 +0700

    Create tailwind-config.js

---
+++ assets/css/tailwind-config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2F4F4F',
+        secondary: '#F5F3E8'
+      },
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+        inter: ['Inter', 'sans-serif']
+      }
+    }
+  }
+};
