f69d20d33e4fc7d2de313cefee5d2a1357818277
bernadusbayu148-source <bernadus.bayu148@gmail.com>
2026-04-29 16:48:15 +0700

    Create tailwind-config.js

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,36 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'tertiary-fixed': '#cbedb4',
+        'on-tertiary': '#ffffff',
+        'on-primary-fixed-variant': '#1e5128',
+        'surface-container-low': '#f6f3f0',
+        'surface-bright': '#fcf9f6',
+        'primary-fixed': '#b8f1b9',
+        'surface-variant': '#e5e2df',
+        'outline-variant': '#c1c9bd',
+        'primary': '#013a13',
+        'secondary-fixed': '#f6decb',
+        'surface-container-highest': '#e5e2df',
+        'surface-container-high': '#f3efeb',
+        'on-background': '#1c1c1a',
+        'primary-container': '#1e5128',
+        'on-primary': '#ffffff',
+        'on-primary-container': '#ffffff',
+        'surface-container': '#f0edea',
+        'surface': '#fcf9f6',
+        'background': '#fcf9f6',
+        'on-surface': '#1c1c1a',
+        'on-surface-variant': '#5f5a56',
+        'secondary': '#6d5b4c'
+      },
+      fontFamily: {
+        headline: ['Newsreader', 'serif'],
+        body: ['Manrope', 'sans-serif'],
+        label: ['Manrope', 'sans-serif']
+      }
+    }
+  }
+};
